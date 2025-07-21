c5bd5da7e4e2899ce274879c7afcfa44348fbeb4
supernovabee <dosin.1ne@gmail.com>
2025-07-20 23:05:58 -0500

    added my kind of crummy text based game.

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -42,6 +42,39 @@ if (choice === "mountains" && hasTorch) {
   console.log("You get lost and wander aimlessly.");
 }
 
+const hasPotion = true;
+const hasCompass = true;
+
+console.log("A new day has dawned. It is time to complete your journey.");
+
+const choice2 = readline.question("Do you drink a potion from your bag? [Yes] or [No]");
+
+if (choice2 === "Yes" && hasPotion) {
+  console.log("The potion teleports you to a bustling market square.");
+} else if (choice2 === "Yes" && !hasPotion) {
+  console.log("Looks like you forgot to pack your potion before leaving.");
+} else if (choice2 === "No" || hasCompass) {
+  console.log("You use your Compass to head towards the west; a direction you know leads to a village.");
+} else {
+  console.log("You get lost and wander aimlessly.");
+}
+
+const hasCoin = false;
+const theivery = false;
+
+console.log("The market square of the village is surrounded by shops while the streets are filled with market stalls.");
+
+const choice3 = readline.question("Do you shop the storefronts or stalls? [storefront] or [stalls]");
+
+if (choice3 === "storefront" && hasCoin) {
+  console.log("You head into the nearest storefront to peruse their wares. You find a neat shiny rock and decide to purchase it for 1 copper.");
+} else if (choice3 === "Yes" && !hasCoin) {
+  console.log("You realize you left your purse at home, but window shopping is fun too");
+} else if (choice3 === "stalls" || theivery) {
+  console.log("You realise you left your purse at home but desire a snack. You snatch an apple from a nearby fruit peddler");
+} else {
+  console.log("You realise you left your purse at home but desire a snack. You snatch an apple from a nearby fruit peddler; however a constable sees you do this. You are chased out of the village with threats of violence if you return.");
+}
 /* 
 
 Add Customization and expand the game:
